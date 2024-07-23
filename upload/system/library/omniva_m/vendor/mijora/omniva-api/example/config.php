09a2185614b01b0f09105f0b942ca8a2edb7375f
Rimvydas V <lithstud@gmail.com>
2024-07-23 17:01:48 +0300

    Dependencies update

--- upload/system/library/omniva_m/vendor/mijora/omniva-api/example/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$username = ''; //API username
-$password = ''; //API password
-$barcodes = []; //tracking numbers for testing
-$email = ''; // email to test label downloading to given email.
--- upload/system/library/omniva_m/vendor/mijora/omniva-api/overrides/tcpdf/tcpdf_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* Enable overrides */
-define('K_TCPDF_EXTERNAL_CONFIG', true);
-
-/* Allow generate barcode image */
-define('K_TCPDF_CALLS_IN_HTML', true);
-define('K_ALLOWED_TCPDF_TAGS', '|write1DBarcode|');
