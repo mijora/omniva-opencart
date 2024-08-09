dbf8f9095cded317af4b66b393e17950d4f16abc
Marijus Kundelis <markakk@gmail.com>
2024-08-09 17:28:01 +0300

    Add Order ID to manifest

--- upload/admin/controller/extension/module/omniva_m.php
+++ upload/admin/controller/extension/module/omniva_m.php
@@ -723,6 +723,7 @@ class ControllerExtensionModuleOmnivaM extends Controller
             foreach ($barcodes as $barcode) {
                 $order = new ApiOrder();
                 $order->setTracking($barcode);
+                $order->setOrderNumber($order_id);
                 $order->setQuantity(1);
                 $order->setWeight($parcel_weight);
                 $order->setReceiver($receiver_address);
@@ -781,6 +782,7 @@ class ControllerExtensionModuleOmnivaM extends Controller
             foreach ($barcodes as $barcode) {
                 $order = new ApiOrder();
                 $order->setTracking($barcode);
+                $order->setOrderNumber($order_id);
                 $order->setQuantity(1);
                 $order->setWeight($parcel_weight);
                 $order->setReceiver($receiver_address);
