516bb48e87dcae1dacdc7cadcc77483c694380a3
Marijus Kundelis <markakk@gmail.com>
2025-02-12 18:48:03 +0200

    Add box calculator library

--- upload/system/library/omniva_m/vendor/composer/autoload_psr4.php
+++ upload/system/library/omniva_m/vendor/composer/autoload_psr4.php
@@ -9,4 +9,5 @@ return array(
     'setasign\\Fpdi\\' => array($vendorDir . '/setasign/fpdi/src'),
     'Mijora\\Omniva\\' => array($vendorDir . '/mijora/omniva-api/src'),
     'Mijora\\OmnivaOpencart\\' => array($baseDir . '/src'),
+    'Mijora\\BoxCalculator\\' => array($vendorDir . '/mijora/box-calculator/src'),
 );
